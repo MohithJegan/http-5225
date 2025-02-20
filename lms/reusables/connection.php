59379001c9eb3911825a5bcf608ec9f6407a4a16
Mohith J <mohithjegan@gmail.com>
2025-02-20 09:01:55 -0500

    Added LMS

---
+++ lms/reusables/connection.php
@@ -0,0 +1,10 @@
+<?php
+                $connect = mysqli_connect('localhost', 'root', '', 'schools');
+
+                if(!$connect){
+                  echo 'Error Code: ' . mysqli_connect_errno();
+                  echo 'Error Message: ' . mysqli_connect_error();
+                  exit;
+                } 
+
+              ?>
