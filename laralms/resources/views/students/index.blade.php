955b907e37e60b2464d47f1d03a4d265c8cc822d
Mohith J <mohithjegan@gmail.com>
2025-04-01 17:46:59 -0400

    Add Create and Update method in the Laravel

--- laralms/resources/views/students/index.blade.php
+++ laralms/resources/views/students/index.blade.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>All Students</h1>
-
+@extends('layout')
+@section('content')
+<h1>All Students</h1>
+<ul>
     @foreach ($students as $student)
-    {{ $student -> fname}} <br>
+       <li>{{ $student -> fname }} {{ $student -> lname }} | <a href="{{route('students.edit', $student -> id)}}">Edit</a> </li></br>
     @endforeach
-
-
-</body>
-</html>
+</ul>
+@endsection
